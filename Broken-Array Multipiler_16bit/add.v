675aea4faf02078c7bd215e80a49fdeb2a4c7d16
stopaimme <56530653+stopaimme@users.noreply.github.com>
2022-03-09 17:16:14 +0800

    Add files via upload

---
+++ Broken-Array Multipiler_16bit/add.v
@@ -0,0 +1,12 @@
+module add(
+    input   A,
+    input   B,
+    input   C,
+    output  Y,
+    output  C1
+    );
+    
+    assign Y=A^B^C;
+    assign C1=(A^B)&C|A&B;
+    
+endmodule
---
+++ Broken-Array Multipiler_16bit/mul_one.v
@@ -0,0 +1,14 @@
+module mul_one(
+    input X,
+    input Y,
+    input Cin,
+    input Z,
+    output S,
+    output Cout
+    );
+    
+    wire xy;
+    assign xy=X & Y;
+    add u0(xy,Z,Cin,S,Cout);
+
+endmodule
